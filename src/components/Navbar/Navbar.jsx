900b963a6ae97ca577d9377e4b14cfae350120d0
madhavgupta07 <madhavgupta5555@gmail.com>
2024-04-04 16:08:42 +0530

    initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Navbar.css";
+export default function Navbar(props) {
+  return (
+    <div className="navbar">
+      <h2>Kanban Board</h2>
+      <div>
+        <input
+          type="checkbox"
+          className="checkbox"
+          id="checkbox"
+          style={{ transition: "all 200ms" }}
+          onChange={props.switchTheme}
+        />
+        <label for="checkbox" class="label">
+          <div className="ball" />
+        </label>
+      </div>
+      {/* <button>Switch theme</button> */}
+    </div>
+  );
+}
